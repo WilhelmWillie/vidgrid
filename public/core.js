c7b4459cc6f2b6af810f6a86d056f0430ec54c97
Wilhelm Willie <wilhelmwillie@gmail.com>
2014-10-11 19:18:42 -0400

    Set up front end interface

---
+++ public/core.js
@@ -0,0 +1,50 @@
+// Core.js
+// Angular fun :-)
+
+var vidgrid = angular.module('vidGrid', []);
+
+function mainController($scope, $http) {
+	$scope.formData = {};
+
+	$scope.hasMsg = false // Used for showing post error messages
+
+	$scope.loadVideos = function() {
+		$http.get('/api/videos')
+			.success(function(data) {
+				$scope.videos = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
+	$scope.createVideo = function() {
+		$http.post('/api/videos', $scope.formData)
+			.success(function(data) {
+				// Success = The response got through.. However, it doesn't mean the video was posted succesfully
+
+				if (data.error) { // We got an error on our hands :-)
+					$scope.flashMsg = "Error: " + data.error;
+					$scope.hasMsg = true;
+				} else {
+					$scope.formData = {};
+					$scope.loadVideos ();
+					$scope.hasMsg = false;
+				}
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
+	$scope.getEmbedUrl = function(id) {
+		return '//www.youtube.com/embed/' + id;
+	}
+
+	$scope.loadVideos ();
+}
+
+vidgrid.filter('trustAsResourceUrl', ['$sce', function($sce) {
+	return function(val) { return $sce.trustAsResourceUrl(val);};
+}]);
--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.use(methodOverride());
 var VideoSchema = new mongoose.Schema({
 	title: String,
 	desc: String,
-	url: String,
-	votes: {Type: Number, min: 0, default: 0},
+	video_id: String,
+	votes: {Type: Number, default: 0},
 	added: {type: Date, default: Date.now}
 });
 
@@ -41,39 +41,55 @@ app.get('/api/videos', function(req, res) { // Grab all the videos in database
 	Video.find(function(err, videos) {
 		// If we stumble upon an error, we send those errors instead of a JSON file
 		if (err) 
-			res.send(err);
-
-		// If no error, we send Videos in JSON format
-		res.json(videos);
+			res.send({"error":err});
+		else 
+			res.json(videos); // If no error, we send Videos in JSON format
 	});
 });
 
 app.post('/api/videos', function(req, res) { // Called when we post up a new video
-	Video.create({
-		title: req.body.title,
-		desc: req.body.desc,
-		url: req.body.url,
-	}, function(err, video) {
-		// Again, if we stumble upon an error, we send that instead
-		if (err)
-			res.send(err);
-		
-		// Return a success message after we create a video
-		res.json({"message":"Video posted!"});
-	});
+	// Before we post the video, we must strip the http/https from the URL!
+	var video_id = youtube_parser(req.body.url);
+
+	if (video_id == "Bad URL") { // If the URL is invalid, then we send back an error message
+		res.send({"error":"Invalid URL!"});
+	} else {
+		Video.create({
+			title: req.body.title,
+			desc: req.body.desc,
+			video_id: video_id,
+			votes: 0
+		}, function(err, video) {
+			// Again, if we stumble upon an error, we send that instead
+			if (err)
+				res.send({"error":err});
+			else
+				res.send({"success":"Video succesfully posted"}); // If we reach this point, posting was a success. We send a success message instead
+		});
+	}
 });
 
+/* NOTE: Delete hasn't been implemented yet!
 app.delete('/api/videos/:video_id', function(req, res) { // Deletes video
 	Video.remove({
 		_id: req.params.video_id
 	}, function(err, video) {
 		if (err)
 			res.send(err);
-
-		// Return a success message after we delete a video
-		res.json({"message":"Video deleted!"});
 	});
 });
+*/
+
+// Helper functions
+function youtube_parser(url) {
+	var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+	var match = url.match(regExp);
+	if (match&&match[7].length == 11) {
+		return match[7];
+	} else {
+		return "Bad URL";
+	}
+}
 
 // Application front end
 app.get('*', function(req, res) {
